2d22415f63559b5a52514e52517e3938147bbe79
Shubhankar Seth <shubhseth@gmail.com>
2022-04-27 16:04:47 +0530

    Done

--- lab2/IntList/Primes.java
+++ lab2/IntList/Primes.java
@@ -7,16 +7,16 @@ public class Primes {
      * otherwise False. When you're debugging, stepping into this function may
      * not be the best idea! Consider instead stepping *over* this function,
      * and ensuring that its return value makes sense.
-     *
+     * <p>
      * If you're curious, this algorithm uses Fermat's Little Theorem as a
      * primality test, and returns the correct answer w.h.p. (due to the presence)
      * of Carmichael numbers. If this makes no sense to you, good! It shouldn't.
      * The goal of this function is to make sure you learn to abstract away the inner
      * workings of a function and debug it as a black-box with the "Step Over" feature.
      *
-     * @source: https://www.geeksforgeeks.org/primality-test-set-2-fermet-method/
      * @param n an arbitary integrer
      * @return True iff. the integer is prime
+     * @source: https://www.geeksforgeeks.org/primality-test-set-2-fermet-method/
      */
     public static boolean isPrime(int n) {
         // Corner cases
@@ -24,11 +24,10 @@ public class Primes {
         if (n <= 3) return true;
 
         int k = 3; // Try k = 3 times
-        while (k > 0)
-        {
+        while (k > 0) {
             // Pick a random number in [2..n-2]
             // Above corner cases make sure that n > 4
-            int a = 2 + (int)(Math.random() % (n - 4));
+            int a = 2 + (int) (Math.random() % (n - 4));
 
             // Fermat's little theorem
             if (power(a, n - 1, n) != 1)
@@ -46,16 +45,14 @@ public class Primes {
      *
      * @source: https://www.geeksforgeeks.org/primality-test-set-2-fermet-method/
      */
-    static int power(int a, int n, int p)
-    {
+    static int power(int a, int n, int p) {
         // Initialize result
         int res = 1;
 
         // Update 'a' if 'a' >= p
         a = a % p;
 
-        while (n > 0)
-        {
+        while (n > 0) {
             // If n is odd, multiply 'a' with result
             if ((n & 1) == 1)
                 res = (res * a) % p;
@@ -67,7 +64,9 @@ public class Primes {
         return res;
     }
 
-    /** Driver Code */
+    /**
+     * Driver Code
+     */
     public static void main(String[] args) {
         /* Print the first 20 primes */
         int primeCount = 0;
--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -1,6 +1,7 @@
 package IntList;
 
 import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class SquarePrimesTest {
@@ -15,6 +16,46 @@ public class SquarePrimesTest {
         IntList lst = IntList.of(14, 15, 16, 17, 18);
         boolean changed = IntListExercises.squarePrimes(lst);
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
+        assertTrue(changed);
+    }
+
+    @Test
+    public void test0() {
+        IntList lst = IntList.of(1, 4, 6, 8, 10);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("1 -> 4 -> 6 -> 8 -> 10", lst.toString());
+        assertFalse(changed);
+    }
+
+    @Test
+    public void test1() {
+        IntList lst = IntList.of(2, 3, 5, 7);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 9 -> 25 -> 49", lst.toString());
+        assertTrue(changed);
+    }
+
+    @Test
+    public void test2() {
+        IntList lst = IntList.of(3, 5, 7, 11);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("9 -> 25 -> 49 -> 121", lst.toString());
+        assertTrue(changed);
+    }
+
+    @Test
+    public void test3() {
+        IntList lst = IntList.of(0, 1, 22, 34, 51, 11, 17, 2, 5);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("0 -> 1 -> 22 -> 34 -> 51 -> 121 -> 289 -> 4 -> 25", lst.toString());
+        assertTrue(changed);
+    }
+
+    @Test
+    public void test4() {
+        IntList lst = IntList.of(1, 11, 22, 3, 51, 2, 17, 16, 5);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("1 -> 121 -> 22 -> 9 -> 51 -> 4 -> 289 -> 16 -> 25", lst.toString());
         assertTrue(changed);
     }
 }
